1f844ccc1cb616d3d1f28578f9b3f5f2c41b703e
Lamyaa439 <lamyaaalghaihab@gmail.com>
2025-11-23 12:26:47 +0300

    create checkbook.py file

---
+++ debugging/checkbook.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+class Checkbook:
+    def __init__(self):
+        self.balance = 0.0
+
+    def deposit(self, amount):
+        self.balance += amount
+        print("Deposited ${:.2f}".format(amount))
+        print("Current Balance: ${:.2f}".format(self.balance))
+
+    def withdraw(self, amount):
+        if amount > self.balance:
+            print("Insufficient funds to complete the withdrawal.")
+        else:
+            self.balance -= amount
+            print("Withdrew ${:.2f}".format(amount))
+            print("Current Balance: ${:.2f}".format(self.balance))
+
+    def get_balance(self):
+        print("Current Balance: ${:.2f}".format(self.balance))
+
+def main():
+    cb = Checkbook()
+    while True:
+        action = input("What would you like to do? (deposit, withdraw, balance, exit): ")
+        action = action.lower()
+        
+        if action == 'exit':
+            break
+            
+        elif action in ['deposit', 'withdraw']:
+            try:
+                prompt_text = "Enter the amount to {}: $".format(action)
+                amount_input = input(prompt_text)
+                amount = float(amount_input)
+                
+                if amount < 0:
+                    print("Amount must be a positive number.")
+                    continue
+                    
+                if action == 'deposit':
+                    cb.deposit(amount)
+                else:
+                    cb.withdraw(amount)
+                    
+            except ValueError:
+                print("Invalid amount entered. Please enter a number.")
+                
+        elif action == 'balance':
+            cb.get_balance()
+            
+        else:
+            print("Invalid command. Please try again.")
+
+if __name__ == "__main__":
+    main()
